ab9de2c8116b6a665f935b5a0bd4f3469f1a0c46
Stevan Earl <stevan.earl@asu.edu>
2022-12-08 16:10:55 -0700

    feat: refactor write raster and create spatial raster

--- R/create_spatialRaster.R
+++ R/create_spatialRaster.R
@@ -1,49 +1,47 @@
 #' @title generate a EML entity of type spatialRaster
 #'
-#' @description create_spatialRaster generates a EML entity of type
-#'   spatialRaster
+#' @description \code{create_spatialRaster} generates a EML entity of type
+#' spatialRaster
 #'
 #' @details a spatialRaster entity is created from a single data file (e.g.,
 #' CAP_1985.img). The resulting entity is renamed with the project id + base
 #' file name + file extension or base file name + file extension depending on
 #' desired output.
 #'
-#' @note create_spatialRaster will look for a package number in config.yaml;
-#' congig.yaml::packageNum must exist in the project directory.
+#' @note \code{create_spatialRaster} will look for a package number in
+#' config.yaml; congig.yaml::packageNum must exist in the project directory.
 #'
 #' @note EML requires that geographic extents are provided as decimal degrees.
 #' Because it is often impractical or inadviseable to change the projection of
 #' rasters, a spatial coverage is not constructed if the projection of a raster
 #' is in units of meters.
 #'
-#' @param rasterFile
+#' @param raster_file
 #'  (character) Quoted full path to raster file.
 #' @param description
 #'  (character) Description of the raster.
-#' @param epsgProjection
+#' @param epsg
 #'  (integer) Four- or five-digit EPSG numeric code of raster Coordinate
 #'  Reference System (CRS)
-#' @param rasterValueDescription
+#' @param raster_value_description
 #'  (character) Description of raster values
-#' @param rasterValueUnits
+#' @param raster_value_units
 #'  (character) Raster value units. Units must be EML-compliant or annotated by
 #'  a custom unit definition.
-#' @param geoDescription
+#' @param geographic_description
 #'  (character) A textual description of the geographic study area of the
-#'  raster. This parameter allows the user to overwrite the
-#'  geographicDesciption value provided in the project config.yaml.
-#' @param projectNaming
+#'  raster. This parameter allows the user to overwrite the study-wide
+#'  geographic_description value provided in config.yaml (default).
+#' @param project_naming
 #'  (logical) Logical indicating if the raster file should be renamed per the
-#'  style used by the CAP LTER (default) with the project id + base file name +
-#'  file extension. The passed file or directory name will be used if this
-#'  parameter is set to FALSE.
+#'  style used by the CAP LTER: project id + base file name + file extension.
 #' @param file_url
 #'  (character) Optional parameter detailing the online location where the data
 #'  entity can be accessed. In most cases, the online location is the same for
-#'  all files in the dataset and is detailed in config.yaml. file_url allows
-#'  the user to overide project-level configuration in config.yaml and provide
-#'  a specific, unique resource (e.g., a link to a file in Dropbox) for each
-#'  data entity.
+#'  all files in the dataset and is detailed in config.yaml. \code{file_url}
+#'  allows the user to overide project-level configuration in config.yaml and
+#'  provide a specific, unique resource (e.g., a link to a file in Dropbox) for
+#'  each data entity.
 #'
 #' @import EML
 #' @importFrom raster raster extent ncell crs bandnr nrow ncol xres yres
@@ -52,8 +50,8 @@
 #' @importFrom yaml yaml.load_file
 #' @importFrom capeml read_package_configuration
 #'
-#' @return EML spatial data object is returned. Additionally, if projectNaming
-#' is set to TRUE (default) the spatial data file is renamed with the project
+#' @return EML spatial data object is returned. Additionally, if project_naming
+#' is set to TRUE the spatial data file is renamed with the project
 #' id + base file name + file extension (zip in the case when multiple files
 #' are aggregated).
 #'
@@ -70,28 +68,28 @@
 #' region" # supercedes yaml
 #'
 #' NAIP_NDVI_2015_SV <- capemlGIS::create_spatialRaster(
-#'    rasterFile             = "path-to-file/NAIP_NDVI_2015.tiff",
-#'    description            = rasterDesc,
-#'    epsgProjection         = 4326,
-#'    rasterValueDescription = "Normalized Difference Vegetation Index (NDVI)",
-#'    rasterValueUnits       = "dimensionless",
-#'    geoDescription         = "my_area",
-#'    projectNaming          = FALSE
+#'    raster_file               = "path-to-file/NAIP_NDVI_2015.tiff",
+#'    description               = rasterDesc,
+#'    epsg                      = 4326,
+#'    raster_value_description  = "Normalized Difference Vegetation Index (NDVI)",
+#'    raster_value_units        = "dimensionless",
+#'    geographic_description    = "my_area",
+#'    project_naming            = FALSE
 #'  )
 #'
 #' }
 #'
 #' @export
-
+#'
 create_spatialRaster <- function(
-  rasterFile,
+  raster_file,
   description,
-  epsgProjection,
-  rasterValueDescription,
-  rasterValueUnits,
-  geoDescription = NULL,
-  projectNaming  = TRUE,
-  file_url       = NULL
+  epsg,
+  raster_value_description,
+  raster_value_units,
+  geographic_description = NULL,
+  project_naming         = FALSE,
+  file_url               = NULL
   ) {
 
 
@@ -110,74 +108,75 @@ create_spatialRaster <- function(
   }
 
   # do not proceed if a description of the raster values is not provided
-  if (missing("rasterValueDescription")) {
+  if (missing("raster_value_description")) {
 
     stop("please provide a desription of the raster cell values")
 
   }
 
   # do not proceed if a epsg of EML-compliant projection is not provided
-  if (missing("epsgProjection")) {
+  if (missing("epsg")) {
 
     stop("please provide a EPSG projection for this raster")
 
   }
 
 
-  # load raster -------------------------------------------------------------
+  # load raster ----------------------------------------------------------------
 
-  raster_object <- raster::raster(rasterFile)
-
-
-  # establish raster file parent directory ----------------------------------
-
-  directory_name <- dirname(rasterFile)
+  this_raster <- suppressWarnings(raster::raster(raster_file))
+  entity_name <- basename(tools::file_path_sans_ext(raster_file))
 
 
-  # build attribute table ---------------------------------------------------
+  # attributes -----------------------------------------------------------------
 
-  rasterFactorsFileName <- paste0(directory_name, "/", basename(file_path_sans_ext(rasterFile)), "_factors.csv")
+  # raster factor yaml metadata exists:
 
-  # compile components for attributeList
+  raster_factors_file_name <- paste0(entity_name, "_factors.yaml")
 
-  # condition: factors present
-  if (file.exists(rasterFactorsFileName)) {
+  if (file.exists(raster_factors_file_name)) {
 
-    rasterAttributes <- data.frame(
-      attributeName       = "raster_value",
-      attributeDefinition = rasterValueDescription
+    raster_factors <- yaml.load_file(raster_factors_file_name) |>
+      yaml::yaml.load() |>
+      tibble::enframe() |>
+      tidyr::unnest_wider(value) |>
+      tidyr::unnest_wider(attribute) |>
+      tidyr::unnest_longer(levels) |>
+      tidyr::unnest_wider(levels) |>
+      dplyr::select(-one_of("name"))
+
+    raster_attributes <- data.frame(
+      attributeName       = raster_factors[["attributeName"]][[1]]
+      attributeDefinition = raster_value_description
     )
 
-    rasterFactors <- utils::read.csv(rasterFactorsFileName)
-
     attr_list <- EML::set_attributes(
-      attributes  = rasterAttributes,
-      factors     = rasterFactors,
+      attributes  = raster_attributes,
+      factors     = raster_factors,
       col_classes = "factor"
     )
 
-    # condition: factors not present (presuming that vars are not categorical)
-
   } else {
 
-    # do not proceed if the units for the rater values is not provided
-    if (missing("rasterValueUnits")) { stop("please provide units for the raster cell values") }
+  # raster factor yaml metadata exists (not categorical):
+
+    # require units
+    if (missing("raster_value_units")) { stop("missing units for raster cell values") }
 
     # build base attributes data frame
-    rasterAttributes <- data.frame(
+    raster_attributes <- data.frame(
       attributeName       = "raster_value",
-      attributeDefinition = rasterValueDescription,
-      unit                = rasterValueUnits
+      attributeDefinition = raster_value_description,
+      unit                = raster_value_units
     )
 
-    # determine raster value number type
-    # this code is run only if the raster is a reasonable size (<= 500 Mb)
-    if (file.size(rasterFile) <= 524288000) {
+    # determine raster value number type (run only if file <= 500 Mb)
 
-      # determine raster number type
-      # sample of raster values (20% of values sans NAs)
-      rasterValuesSample <- na.omit(sample(raster_object, size = 0.2 * raster::ncell(raster_object)))
-      rasterValuesSample <- rasterValuesSample[is.finite(rasterValuesSample)] # remove infs (just in case)
+    if (file.size(raster_file) <= 524288000) {
+
+      # determine raster number type by sampling 20% of values sans NAs
+      rasterValuesSample <- na.omit(sample(this_raster, size = 0.2 * raster::ncell(this_raster)))
+      rasterValuesSample <- rasterValuesSample[is.finite(rasterValuesSample)]
 
       rounded <- floor(rasterValuesSample)
 
@@ -199,65 +198,66 @@ create_spatialRaster <- function(
 
       }
 
-      rasterAttributes$numberType <- rasterNumberType
+      raster_attributes$numberType <- rasterNumberType
 
     } else {
 
-      rasterAttributes$numberType <- "real"
+      raster_attributes$numberType <- "real"
 
     } # close raster value number type
 
     attr_list <- EML::set_attributes(
-      attributes  = rasterAttributes,
+      attributes  = raster_attributes,
       col_classes = "numeric"
     )
 
   } # close condition: factors not present
 
-  # add additionalInfo - projections ----------------------------------------
+
+  # additionalInfo ------------------------------------------------------------
+
+  this_crs <- raster::crs(this_raster)
 
   projections <- list(
     section = list(
-      paste0("<title>user-provided coordinate reference system</title>\n<para>", epsgProjection, "</para>"),
-      paste0("<title>raster-derived coordinate reference system</title>\n<para>", as.character(raster::crs(raster_object)), "</para>")
+      paste0("<title>user-provided coordinate reference system</title>\n<para>", epsg, "</para>"),
+      paste0("<title>raster-derived coordinate reference system</title>\n<para>", as.character(this_crs), "</para>")
     )
   )
 
   message(
-    paste0("user-provided CRS: ", epsgProjection, "\n"),
-    paste0("raster-derived CRS: ", as.character(raster::crs(raster_object)))
+    paste0("user-provided CRS: ", epsg, "\n"),
+    paste0("raster-derived CRS: ", as.character(this_crs)))
   )
+
 
   # identify EML-compliant spatial reference --------------------------------
 
-  if (!epsgProjection %in% capemlGIS::eml_valid_crs$epsg) {
+  if (!epsg %in% capemlGIS::eml_valid_crs$epsg) {
 
-    stop("cannot identify EML-compliant projection for EPSG: ", epsgProjection)
+    stop(epsg, " is not a EML-compliant projection")
 
   }
 
-  emlProjection <- capemlGIS::eml_valid_crs[capemlGIS::eml_valid_crs$epsg == epsgProjection,]$value
+  emlProjection <- capemlGIS::eml_valid_crs[capemlGIS::eml_valid_crs$epsg == epsg,]$value
 
 
   # create spatial raster entity --------------------------------------------
 
-  # set authentication (md5)
   fileAuthentication                <- EML::eml$authentication(method = "MD5")
-  fileAuthentication$authentication <- md5sum(rasterFile)
+  fileAuthentication$authentication <- md5sum(raster_file)
 
-  # set file size
   fileSize      <- EML::eml$size(unit = "byte")
-  fileSize$size <- deparse(file.size(rasterFile))
+  fileSize$size <- deparse(file.size(raster_file))
 
-  # set file format
   fileDataFormat <- EML::eml$dataFormat(
-    externallyDefinedFormat = EML::eml$externallyDefinedFormat(formatName = file_ext(rasterFile))
+    externallyDefinedFormat = EML::eml$externallyDefinedFormat(formatName = file_ext(raster_file))
   )
 
-  rasterBaseName    <- basename(rasterFile)
-  directory_name    <- dirname(rasterFile)
+  rasterBaseName    <- basename(raster_file)
+  directory_name    <- dirname(raster_file)
   directoryNameFull <- sub("/$", "", path.expand(directory_name))
-  pathToFile        <- path.expand(rasterFile)
+  pathToFile        <- path.expand(raster_file)
 
 
   # retrieve dataset details from config.yaml
@@ -265,38 +265,31 @@ create_spatialRaster <- function(
   configurations <- capeml::read_package_configuration()
 
 
-  if (projectNaming == TRUE) {
+  # project naming
 
-    # if using project naming, add project-name specific elements to
-    # spatialRaster entity
+  if (project_naming == TRUE) {
 
     newRasterName <- paste0(
       configurations$identifier, "_",
       file_path_sans_ext(rasterBaseName), ".",
-      file_ext(rasterFile)
+      file_ext(raster_file)
     )
 
     newRasterNameDir <- paste0(directoryNameFull, "/", newRasterName)
 
     file.copy(
-      from = rasterFile,
+      from = raster_file,
       to   = newRasterNameDir
     )
 
-    # identifier
     resource_ident <- newRasterName
-
-    # close projectNaming == TRUE
 
   } else {
 
-    # if not using not project naming, add source-name specific elements to
-    # spatialRaster entity
-
-    # identifier
     resource_ident <- rasterBaseName
 
-  } # close projectNaming == FALSE
+  }
+
 
   # distribution
 
@@ -317,89 +310,87 @@ create_spatialRaster <- function(
   }
 
 
-# build physical
+  # build physical
 
-spatialRasterPhysical <- EML::eml$physical(
-  objectName     = resource_ident,
-  authentication = fileAuthentication,
-  size           = fileSize,
-  dataFormat     = fileDataFormat,
-  distribution   = fileDistribution
-)
+  spatialRasterPhysical <- EML::eml$physical(
+    objectName     = resource_ident,
+    authentication = fileAuthentication,
+    size           = fileSize,
+    dataFormat     = fileDataFormat,
+    distribution   = fileDistribution
+  )
 
-# build spatialRaster
+  # build spatialRaster
 
-newSR <- EML::eml$spatialRaster(
-  entityName          = resource_ident, # diff
-  entityDescription   = description,
-  physical            = spatialRasterPhysical,
-  additionalInfo      = projections,
-  attributeList       = attr_list,
-  spatialReference    = EML::eml$spatialReference(
-    horizCoordSysName = emlProjection
-    ),
-  numberOfBands       = raster::bandnr(raster_object),
-  rows                = raster::nrow(raster_object),
-  columns             = raster::ncol(raster_object),
-  horizontalAccuracy  = EML::eml$horizontalAccuracy(accuracyReport = "METADATA_NOT_PROVIDED"),
-  verticalAccuracy    = EML::eml$verticalAccuracy(accuracyReport = "METADATA_NOT_PROVIDED"),
-  cellSizeXDirection  = raster::xres(raster_object),
-  cellSizeYDirection  = raster::yres(raster_object),
-  rasterOrigin        = "Upper Left",
-  verticals           = 1,
-  cellGeometry        = "pixel",
-  id                  = resource_ident # diff
-)
+  newSR <- EML::eml$spatialRaster(
+    entityName          = resource_ident,
+    entityDescription   = description,
+    physical            = spatialRasterPhysical,
+    additionalInfo      = projections,
+    attributeList       = attr_list,
+    spatialReference    = EML::eml$spatialReference(
+      horizCoordSysName = emlProjection
+      ),
+    numberOfBands       = raster::bandnr(this_raster),
+    rows                = raster::nrow(this_raster),
+    columns             = raster::ncol(this_raster),
+    horizontalAccuracy  = EML::eml$horizontalAccuracy(accuracyReport = "METADATA_NOT_PROVIDED"),
+    verticalAccuracy    = EML::eml$verticalAccuracy(accuracyReport = "METADATA_NOT_PROVIDED"),
+    cellSizeXDirection  = raster::xres(this_raster),
+    cellSizeYDirection  = raster::yres(this_raster),
+    rasterOrigin        = "Upper Left",
+    verticals           = 1,
+    cellGeometry        = "pixel",
+    id                  = resource_ident
+  )
 
-# add (spatial) coverage --------------------------------------------------
 
-# EML requires that geographic extents are provided as decimal degrees; a
-# spatial coverage is not constructed for rasters is in units of meters.
+  # spatial coverage (units must be DD, not constructed for units = m)
 
-if (abs(raster::extent(raster_object)@xmin) > 180) {
+  if (abs(raster::extent(this_raster)@xmin) > 180) {
 
-  warning("projection in meters, spatial coverage not constructed")
-
-} else {
-
-  if (!is.null(geoDescription)) {
-
-    geoDesc <- configurations[["geographic_description"]]
-
-    if (is.na(geoDesc) | is.null(geoDesc) | geoDesc == "") {
-
-      warning("geographic description provided in config.yaml is empty")
-
-    }
+    warning("projection in meters, spatial coverage not constructed")
 
   } else {
 
-    geoDesc <- geoDescription
+    if (!is.null(geographic_description)) {
+
+      this_geographic_description <- geographic_description
+
+    } else {
+
+      this_geographic_description <- configurations[["geographic_description"]]
+
+      if (is.na(this_geographic_description) | is.null(this_geographic_description) | this_geographic_description == "") {
+
+        this_geographic_description <- "METADATA NOT PROVIDED"
+        warning("geographic description not provided and missing from config")
+
+      }
+
+    }
+
+    this_extent <- raster::extent(this_raster)
+
+    rasterSpatialCoverage <- list(
+      geographicDescription = this_geographic_description,
+      boundingCoordinates   = list(
+        westBoundingCoordinate  = this_extent@xmin,
+        eastBoundingCoordinate  = this_extent@xmax,
+        northBoundingCoordinate = this_extent@ymax,
+        southBoundingCoordinate = this_extent@ymin
+      )
+    )
+
+    newSR$coverage$geographicCoverage <- rasterSpatialCoverage
 
   }
 
-  rasterSpatialCoverage <- list(
-    geographicDescription = geoDesc,
-    boundingCoordinates   = list(
-      westBoundingCoordinate  = raster::extent(raster_object)@xmin,
-      eastBoundingCoordinate  = raster::extent(raster_object)@xmax,
-      northBoundingCoordinate = raster::extent(raster_object)@ymax,
-      southBoundingCoordinate = raster::extent(raster_object)@ymin
-    )
-  )
 
-  newSR$coverage$geographicCoverage <- rasterSpatialCoverage
+  # return -----------------------------------------------------------------------
+
+  message("created spatialRaster: ", resource_ident)
+
+  return(newSR)
 
 }
-
-
-# closing message ---------------------------------------------------------
-
-message(paste0("created spatialRaster: ", resource_ident))
-
-
-# return spatial raster object --------------------------------------------
-
-return(newSR)
-
-} # close create_spatialRaster
